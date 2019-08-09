42afd097b0b4c1c8f42bf2a8e7fe30643fafc515
sundareshan1998 <46001087+Sundareshan98@users.noreply.github.com>
2019-08-09 12:49:27 +0530

    Create alpha.py

---
+++ alpha.py
@@ -0,0 +1,8 @@
+n=str(input())
+l=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+l1=['A','B','C','D','E','F','G','H','J','I','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+if n in l or
+n in l1:
+    print("Alphabet")
+else:
+    print("No")
